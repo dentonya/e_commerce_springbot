0344b403ee342ebb04c58219d799789e2cae1599
den_atonya <denatonya@gmail.com>
2021-07-01 20:25:55 +0300

    e_commerce_webiste
    e_commerce website using springbot

---
+++ src/main/java/ac/ke/jkuat/ecommerce/ecommerce_springboot/EcommerceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package ac.ke.jkuat.ecommerce.ecommerce_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ac/ke/jkuat/ecommerce/ecommerce_springboot/EcommerceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.ke.jkuat.ecommerce.ecommerce_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
